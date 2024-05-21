fb15caac2ebc7316a43388599473d039c0308ea7
leesiwoo <s24028@gsm.hs.kr>
2024-05-21 21:03:40 +0900

    게임 오버 화면 추가, 오디오 추가

--- GSM_RHYTHM/Game.c
+++ GSM_RHYTHM/Game.c
@@ -5,6 +5,8 @@
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
 #include "Example.h"
+#include <stdlib.h>
+#include <time.h>
 
 SDL_Renderer* Renderer = NULL;
 SDL_Window* window = NULL;
@@ -20,8 +22,15 @@ SDL_Texture* fishselete = NULL;
 SDL_Texture* gameback = NULL;
 SDL_Texture* ready = NULL;
 SDL_Texture* go = NULL;
+SDL_Texture* barrier = NULL;
 Mix_Music* music = NULL;
+Mix_Music* gameMusic = NULL;
 Mix_Chunk* StartSound = NULL;
+Mix_Chunk* JumpSound = NULL;
+Mix_Chunk* seleteSound = NULL;
+SDL_Texture* Death = NULL;
+Mix_Chunk* DeathSound = NULL;
+
 
 bool init() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -76,20 +85,28 @@ bool loadMedia() {
     fishgame = IMG_LoadTexture(Renderer, "Image/Game/Fish game.png");
     background = IMG_LoadTexture(Renderer, "Image/Game/background.png");
     start = IMG_LoadTexture(Renderer, "Image/Game/start.png");
-    quit1 = IMG_LoadTexture(Renderer, "Image/game/quit.png");
-    fishselete = IMG_LoadTexture(Renderer, "Image/game/fishopen.png");
+    quit1 = IMG_LoadTexture(Renderer, "Image/Game/quit.png");
+    fishselete = IMG_LoadTexture(Renderer, "Image/Game/fishopen.png");
     gameback = IMG_LoadTexture(Renderer, "Image/Game/gameback.png");
     ready = IMG_LoadTexture(Renderer, "Image/Game/Ready.png");
     go = IMG_LoadTexture(Renderer, "Image/Game/Go.png");
     music = Mix_LoadMUS("Songs/mainSong1.wav");
+    barrier = IMG_LoadTexture(Renderer, "Image/Game/barrier.png");
+    gameMusic = Mix_LoadMUS("Songs/gameSong.wav");
+    Death = IMG_LoadTexture(Renderer, "Image/Game/gameover.png");
+    
 
     if (fish == NULL || startimg == NULL || fishgame == NULL || background == NULL ||
         start == NULL || quit1 == NULL || fishselete == NULL || gameback == NULL ||
-        ready == NULL || go == NULL) {
+        ready == NULL || go == NULL || barrier == NULL || Death == NULL) {
         printf("이미지를 로드하는 데 실패했습니다!\n");
         return false;
     }
     if (music == NULL) {
+        printf("음악을 로드하는 데 실패했습니다!\n");
+        return false;
+    }
+    if (gameMusic == NULL) {
         printf("음악을 로드하는 데 실패했습니다!\n");
         return false;
     }
@@ -103,6 +120,17 @@ bool AudioSound() {
         printf("효과음을 불러오지 못했습니다!\n");
         return false;
     }
+    JumpSound = Mix_LoadWAV("Sound/jump.wav");
+    if (!JumpSound) {
+        printf("효과음을 불러오지 못했습니다!\n");
+        return false;
+    }
+    seleteSound = Mix_LoadWAV("Sound/selete1.wav");
+    if (!seleteSound) {
+        printf("효과음을 불러오지 못했습니다!\n");
+        return false;
+    }
+    DeathSound = Mix_LoadWAV("Sound/death.wav");
     return true;
 }
 
@@ -124,16 +152,24 @@ void close() {
     SDL_DestroyTexture(gameback);
     SDL_DestroyTexture(ready);
     SDL_DestroyTexture(go);
+    SDL_DestroyTexture(barrier);
     TTF_Quit();
     IMG_Quit();
     SDL_Quit();
 }
 
 int y = 100;
+int x = 550;
+//장애물 y값 범위
+int a = -300;
+int b = 0;
+
 
 bool gameover = false;
 
 bool Jump = false;
+
+bool startBarrier = false;
 
 void GravitySetting() { //물고기의 움직임 조절 함수
     y += 2; // 물고기를 떨어뜨리는 코드
@@ -151,6 +187,13 @@ void GravitySetting() { //물고기의 움직임 조절 함수
         }
         
     }
+    // 장애물 움직임 코드
+    x -= 4;
+    if (x < -300) {
+        x = 550;
+        startBarrier = false;
+    }
+    // 물고기 오브젝트를 넘어갈 시 신호를 보냄
 }
 
 int main(int argc, char* argv[]) {
@@ -164,12 +207,16 @@ int main(int argc, char* argv[]) {
     }
     bool startAudio = false;
     bool MusicStart = false;
+    bool gameMusicstart = false;
+    bool gameMusicend = false;
     bool isDestroyed = false;
     bool gameStart = false;
     bool selete = false;
     bool start1 = false;
     bool Ready = true;
     bool readyDelete = false;
+    bool DeathMenu = false;
+    int y1 = 0;
 
     SDL_Event event;
     int quit = 0;
@@ -180,17 +227,24 @@ int main(int argc, char* argv[]) {
                 quit = true;
             }
             if (event.type == SDL_KEYDOWN) {
+                AudioSound();
                 switch (event.key.keysym.sym) {
                 case SDLK_DOWN:
+                    if(gameStart == false)
+                        Mix_PlayChannel(-1, seleteSound, 0);
                     selete = true;
                     break;
                 case SDLK_UP:
+                    if(gameStart == false)
+                        Mix_PlayChannel(-1, seleteSound, 0);
                     selete = false;
                     break;
                 case SDLK_SPACE:
                     start1 = true;
                     break;
                 case SDLK_z:
+                    if(gameStart == true)
+                        Mix_PlayChannel(-1, JumpSound, 0);
                     Jump = true;
                     break;
                 }
@@ -211,8 +265,7 @@ int main(int argc, char* argv[]) {
         }
 
         SDL_RenderClear(Renderer);
-
-        if (isDestroyed == true && gameStart == false) {
+        if (isDestroyed == true && gameStart == false && DeathMenu == false) {
             if (MusicStart == false) {
                 Mix_PlayMusic(music, -1);
                 MusicStart = true;
@@ -244,7 +297,6 @@ int main(int argc, char* argv[]) {
 
             SDL_RenderPresent(Renderer);
         }
-
         if (gameStart == true) {
             AudioSound();
             if (startAudio == false) {
@@ -253,7 +305,7 @@ int main(int argc, char* argv[]) {
                 startAudio = true;
             }
             SDL_RenderCopy(Renderer, gameback, NULL, NULL);
-
+            srand((unsigned)time(NULL));
             if (Ready == true) {
                 stretchTexture(Renderer, 150, y, 100, 100, fish);
                 for (int i = 255; i >= 0; i-=5)
@@ -274,10 +326,40 @@ int main(int argc, char* argv[]) {
                 Ready = false;
             }
             else {
+                if (startBarrier == true) {
+                    y1 = rand() % (b - a + 1) + a;
+                    startBarrier = false;
+                }
+                if (gameMusicstart == false) {
+                    Mix_PlayMusic(gameMusic, -1);
+                    gameMusicstart = true;
+                }
+                    
                 GravitySetting();
                 stretchTexture(Renderer, 150, y, 100, 100, fish);
+                stretchTexture(Renderer, x, y1, 400, 800, barrier);
                 SDL_RenderPresent(Renderer);
             }
+        }
+        if (gameover == true) {
+            if (gameMusicend == false) {
+                Mix_FreeMusic(gameMusic);            
+                Mix_PlayChannel(-1, DeathSound, 0);
+                gameMusicend = true;
+            }
+            gameStart = false;
+            DeathMenu = true;
+            SDL_RenderClear(Renderer);
+
+            SDL_RenderCopy(Renderer, background, NULL, NULL);
+            SDL_RenderCopy(Renderer, fishselete, NULL, NULL);
+            SDL_RenderCopy(Renderer, Death, NULL, NULL);
+            SDL_RenderPresent(Renderer);
+            SDL_Delay(2000);
+            DeathMenu = false;
+            MusicStart = false;
+            gameover = false;
+            printf("게임오버됨!\n");
         }
     }
 
